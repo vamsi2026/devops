7191cea17d5eaa828159a39ef76c2849cec6a00c
vamsi2026 <vamsikrishnateeda2026@gmail.com>
2025-04-03 09:45:12 +0000

    this is version v1

---
+++ nodehealth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+  
+#######################
+# Author : Vamsi
+# Date : 31/03/2025
+#
+# The script otputs the node health
+#
+#
+# version: v1
+# ####################
+
+set -x #debug mode
+
+df -h
+
+free -g
+
+nproc
+
