25c6da652baf7bff796d165e9d0610883498c9ab
vamsi2026 <vamsikrishnateeda2026@gmail.com>
2025-04-07 08:15:23 +0000

    Added gitdemo

---
+++ gitdemo/calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ 
+##############
+#
+#Author: Vamsi
+#
+#Addition of two number
+#
+#Version: v1
+#
+##############
+ 
+a=2
+b=3
+
+c=$((a + b))
+
+echo "Addition of $a and $b is: $c"
